f21b3778ff8a25542a9c56ac9071c1d66268295a
Andrew Howard <andrew.bryce.howard@gmail.com>
2025-02-07 06:56:22 -0500

    Update pushMacros.js
    Sync'd with Generate Users version.

--- pushMacros.js
+++ pushMacros.js
@@ -1,22 +1,20 @@
+async function pushMacros( user, gmMacros ) {
+    let userMacros = user.getHotbarMacros(1);
+    for (let i = 0; i < 10; i++) {
+        let macroDoc = gmMacros[i].macro;
+        let slot = userMacros.find((s) => s.macro == null );
+        if (slot && macroDoc ) {
+            await user.assignHotbarMacro( macroDoc, slot );
+        }
+    }
+}
+
 const gm = game.user;
 const gmMacros = gm.getHotbarMacros(5);
 
 for (const macroDoc of gmMacros) {
-  if (macroDoc.macro !== null) {
-    await macroDoc.macro.update( { 'ownership.default': 2 } );
-  }
-}
-
-for (const user of game.users) {
-  if (user !== gm) {
-    let userMacros = user.getHotbarMacros(1);
-    for (let i = 0; i < 10; i++) {
-      let macroDoc = gmMacros[i].macro;
-      let slot = userMacros.find((s) => s.macro == null );
-      if (slot) {
-          await user.assignHotbarMacro( macroDoc, slot );
-      }
+    if (macroDoc.macro) {
+        macroDoc.macro.update( { 'ownership.default': 2 } );
     }
-  }    
-} 
+}
 ui.notifications.info(`Pushed macros to all users.`)
